8d504cd1ae2bf7355f80b58450137fb5bb9bc517
Wojciech Pedrak <woj123@gmail.com>
2020-04-27 18:15:55 +0200

    4.1 done

---
+++ 04/solution.py
@@ -0,0 +1,25 @@
+import hashlib
+
+
+def get_hash(text):
+    return hashlib.md5(text.encode()).hexdigest()
+
+
+def solve(secret_key):
+    decimal = 0
+
+    while True:
+        if decimal % 1000 == 0:
+            print(decimal)
+        to_hash = secret_key + str(decimal)
+        hashed = get_hash(to_hash)
+        if hashed[:5] == '00000':
+            return decimal
+
+        decimal += 1
+
+
+secret_key = 'bgvyzdsv'
+result = solve(secret_key)
+
+print(result)
