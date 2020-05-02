aa35858a05504532a250afc7c862925016755305
Wojciech Pedrak <woj123@gmail.com>
2020-05-02 16:57:43 +0200

    12.1 done

---
+++ 12/solution.py
@@ -0,0 +1,31 @@
+import json
+
+
+def get_line(filename='12/input.txt'):
+    file = open(filename, 'r')
+    return [line.rstrip('\n') for line in file.readlines()][0]
+
+
+def sum_numbers(json_data):
+    if type(json_data) == int:
+        return json_data
+
+    if type(json_data) == list:
+        result = 0
+        for item in json_data:
+            result += sum_numbers(item)
+        return result
+
+    if type(json_data) == dict:
+        result = 0
+        for item in json_data.values():
+            result += sum_numbers(item)
+        return result
+
+    return 0
+
+
+data = json.loads(get_line())
+result = sum_numbers(data)
+
+print(result)
